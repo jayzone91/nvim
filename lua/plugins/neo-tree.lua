7b44c24e0693cdf1b9cdcead896b140fe778894e
jayzone91 <register@j4yz0n3.de>
2023-12-25 20:38:32 +0100

    image gone / stylua file added

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -6,7 +6,7 @@ return {
       "nvim-lua/plenary.nvim",
       "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
       "MunifTanjim/nui.nvim",
-      "3rd/image.nvim",              -- Optional image support in preview window: See `# Preview Mode` for more information
+      -- "3rd/image.nvim",              -- Optional image support in preview window: See `# Preview Mode` for more information
       {
         "s1n7ax/nvim-window-picker",
         version = "2.*",
@@ -127,7 +127,7 @@ return {
             ["<2-LeftMouse>"] = "open",
             ["<cr>"] = "open",
             ["<esc>"] = "cancel", -- close preview or floating neo-tree window
-            ["P"] = { "toggle_preview", config = { use_float = true, use_image_nvim = true } },
+            ["P"] = { "toggle_preview", config = { use_float = true, use_image_nvim = false } },
             -- Read `# Preview Mode` for more information
             ["l"] = "focus_preview",
             ["S"] = "open_split",
