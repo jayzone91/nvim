e618d62bc5d046e8c196b46ceb02eb75a3009995
jayzone91 <register@j4yz0n3.de>
2023-12-31 16:33:42 +0100

    added testing colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -35,4 +35,41 @@ return {
       vim.cmd.colorscheme("catppuccin")
     end,
   },
+  {
+    -- Testing
+    dir = "D:\\git\\xcad-color.nvim",
+    enabled = false,
+    priority = 1000,
+    -- opts = {
+    --   integrations = {
+    --     treesitter = true,
+    --     neotree = true,
+    --     telescope = {
+    --       enabled = true,
+    --     },
+    --     native_lsp = {
+    --       enabled = true,
+    --       virtual_text = {
+    --         errors = { "italic" },
+    --         hints = { "italic" },
+    --         warnings = { "italic" },
+    --         information = { "italic" },
+    --       },
+    --       underlines = {
+    --         errors = { "underline" },
+    --         hints = { "underline" },
+    --         warnings = { "underline" },
+    --         information = { "underline" },
+    --       },
+    --       inlay_hints = {
+    --         background = true,
+    --       },
+    --     },
+    --   },
+    -- },
+    config = function()
+      require("xcad-color").setup()
+      vim.cmd.colorscheme("xcad-color")
+    end,
+  },
 }
