c96c7f4c0ff3288c80fa5f808207643452cafe5b
Johannes Kirchner (Computer Extra) <service@computer-extra.de>
2024-01-08 09:49:37 +0100

    added right mappings for cmp / added "cowboy" function to force myself into motions / disabled arrow keys

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,23 +1,26 @@
 local map = require("utils.functions").map
+local cowboy = require("utils.functions").cowboy
+
+cowboy()
 
 map("n", "<leader>qq", "<cmd>qa<cr>", "Quit Neovim")
 map({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", "Save File")
 
 -- Dupe Lines
-map("n", "<leader><up>", "YP", "Duplicate Line Up")
-map("n", "<leader><down>", "Yp", "Duplicate Line Down")
+map("n", "<leader>k", "YP", "Duplicate Line Up")
+map("n", "<leader>j", "Yp", "Duplicate Line Down")
 
 -- Move Lines
-map("n", "<a-up>", "<cmd>m .-2<cr>==", "Move Line Up")
-map("n", "<a-down>", "<cmd>m .+1<cr>==", "Move Line Up")
+map("n", "<a-k>", "<cmd>m .-2<cr>==", "Move Line Up")
+map("n", "<a-j>", "<cmd>m .+1<cr>==", "Move Line Up")
 
 -- Split Window
 map("n", "<leader>ss", "<cmd>vsplit<cr>", "Split Screen")
 map("n", "<leader>sv", "<cmd>split<cr>", "Split Screen")
-map({ "i", "n" }, "<C-left>", "<C-w>h", "Go to left Window")
-map({ "i", "n" }, "<C-up>", "<C-w>k", "Go to upper Window")
-map({ "i", "n" }, "<C-down>", "<C-w>j", "Go to lower Window")
-map({ "i", "n" }, "<C-right>", "<C-w>l", "Go to right Window")
+map({ "i", "n" }, "<C-l>", "<C-w>h", "Go to left Window")
+map({ "i", "n" }, "<C-k>", "<C-w>k", "Go to upper Window")
+map({ "i", "n" }, "<C-j>", "<C-w>j", "Go to lower Window")
+map({ "i", "n" }, "<C-h>", "<C-w>l", "Go to right Window")
 map("n", "te", "<cmd>tabedit<cr>", "New Tab")
 map("n", "<Tab>", "<cmd>tabnext<cr>", "Next Tab")
 map("n", "<s-Tab>", "<cmd>prevtab<cr>", "Prev Tab")
@@ -39,3 +42,9 @@ map(
   "Open floating diagnostic message"
 )
 map("n", "<leader>dq", vim.diagnostic.setloclist, "Open diagnostics list")
+
+-- disable arrow keys to better lern vim motions.
+map("n", "<Up>", "", "")
+map("n", "<Down>", "", "")
+map("n", "<Left>", "", "")
+map("n", "<Right>", "", "")
