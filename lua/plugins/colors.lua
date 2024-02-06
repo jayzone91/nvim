8807baf52a4c964f54ca898257e59833a5bd4ab5
computerextra <service@computer-extra.de>
2024-02-06 11:47:07 +0100

    Update colors.lua

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -2,20 +2,21 @@ return {
   "catppuccin/nvim",
   config = function()
     require("catppuccin").setup({
+      transparent_background = true,
+      term_colors = true,
       integrations = {
         cmp = true,
-        -- gitsigns = true,
-        -- harpoon = true,
-        -- illuminate = true,
-        -- indent_blankline = {
-        -- enabled = false,
-        -- scope_color = "sapphire",
-        -- colored_indent_levels = false,
-        -- },
+        gitsigns = true,
+        harpoon = true,
+        illuminate = true,
+        indent_blankline = {
+          enabled = false,
+          scope_color = "sapphire",
+          colored_indent_levels = false,
+        },
         mason = true,
         native_lsp = { enabled = true },
-        -- notify = true,
-        -- symbols_outline = true,
+        notify = true,
         telescope = true,
         treesitter = true,
         treesitter_context = true,
