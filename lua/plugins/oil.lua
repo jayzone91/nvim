00859cd4e849fae2a1b4155b8aa14d6a60bdcf13
jayzone91 <register@j4yz0n3.de>
2024-03-09 09:18:00 +0100

    Anpassungen an oil

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,21 +1,44 @@
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "oil",
+  callback = function() vim.opt_local.colorcolumn = "" end,
+})
+
 return {
   "stevearc/oil.nvim",
   opts = {},
   lazy = false,
   config = function()
     require("oil").setup({
+      keymaps = {
+        ["g?"] = "actions.show_help",
+        ["<CR>"] = "actions.select",
+        ["<C-\\>"] = "actions.select_vsplit",
+        ["<C-enter>"] = "actions.select_split", -- this is used to navigate left
+        ["<C-t>"] = "actions.select_tab",
+        ["<C-p>"] = "actions.preview",
+        ["q"] = "actions.close",
+        ["<C-r>"] = "actions.refresh",
+        ["<BS>"] = "actions.parent",
+        ["_"] = "actions.open_cwd",
+        ["`"] = "actions.cd",
+        ["~"] = "actions.tcd",
+        ["gs"] = "actions.change_sort",
+        ["gx"] = "actions.open_external",
+        ["H"] = "actions.toggle_hidden",
+      },
+      use_default_keymaps = false,
       default_file_browser = true,
 
       columns = {
         "icons",
-        "permissions",
+ --       "permissions",
         "size",
       },
 
       win_options = {
         wrap = false,
-        signcolumn = "yes",
-        cursorcolumn = true,
+        signcolumn = "no",
+        cursorcolumn = false,
         foldcolumn = "0",
         spell = false,
         list = true,
