06e7da601f238e74d8d057b731419bc50a4f92d5
jayzone91 <register@j4yz0n3.de>
2024-05-03 19:29:49 +0200

    alles wech

--- lua/autocmds.lua
+++
@@ -1,6 +0,0 @@
--- Highlight when yanking (copying) text
-vim.api.nvim_create_autocmd("TextYankPost", {
-  desc = "Highlight wen yanking text",
-  group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
-  callback = function() vim.highlight.on_yank() end,
-})
--- lua/plugins/Comment.lua
+++
@@ -1,5 +0,0 @@
-return {
-  -- use "gc" to comment visual regions/lines / use "gcc" to comment in insert
-  "numToStr/Comment.nvim",
-  opts = {},
-}
