da7fca27458c32494f5158d955e64c51d9977ebe
jayzone91 <register@j4yz0n3.de>
2024-05-04 16:07:16 +0200

    Comment configs

--- lua/plugins/Comment.lua
+++ lua/plugins/Comment.lua
@@ -3,5 +3,10 @@ return {
   -- https://github.com/numToStr/Comment.nvim
   "numToStr/Comment.nvim",
   lazy = false,
-  opts = {},
+  config = function()
+    ---@diagnostic disable-next-line:missing-fields
+    require("Comment").setup({
+      pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+    })
+  end,
 }
--- lua/plugins/nvim-ts-context-commentstring.lua
+++ lua/plugins/nvim-ts-context-commentstring.lua
@@ -2,5 +2,13 @@ return {
   -- Neovim treesitter plugin for setting the commentstring based on the cursor location in a file.
   -- https://github.com/JoosepAlviste/nvim-ts-context-commentstring
   "JoosepAlviste/nvim-ts-context-commentstring",
-  opts = {},
+  opts = {
+    enable_autocmd = false,
+    languages = {
+      templ = {
+        __default = "// %s",
+        component_declaration = "<!-- %s -->",
+      },
+    },
+  },
 }
