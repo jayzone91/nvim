c1ebb35ada43dee25b901ec6c15b84f8a3e97bb3
jayzone91 <register@j4yz0n3.de>
2024-05-04 16:00:05 +0200

    rose pine lualine

--- lua/plugins/rose-pine.lua
+++ lua/plugins/rose-pine.lua
@@ -6,7 +6,42 @@ return {
   priority = 1000,
   opts = {
     variant = "moon",
+    dark_variant = "moon",
+    bold_vert_split = false,
+    dim_nc_background = false,
+    disable_background = true,
+    disable_float_background = true,
+    disable_italics = false,
+
+    groups = {
+      background = "NONE",
+      background_nc = "_experimental_nc",
+      panel = "surface",
+      panel_nc = "base",
+      border = "highlight_med",
+      comment = "muted",
+      link = "iris",
+      puncuation = "subtle",
+
+      error = "love",
+      hint = "iris",
+      info = "foam",
+      warn = "gold",
+
+      headings = {
+        h1 = "iris",
+        h2 = "foam",
+        h3 = "rose",
+        h4 = "gold",
+        h5 = "pine",
+        h6 = "foam",
+      },
+    },
+
     highlight_groups = {
+      ColorColumn = { bg = "NONE" },
+      CursorLine = { bg = "NONE", blend = 10 },
+      Search = { bg = "muted", inherit = false },
       TelescopeBorder = { fg = "highlight_high", bg = "none" },
       TelescopeNormal = { bg = "none" },
       TelescopePromptNormal = { bg = "base" },
@@ -14,8 +49,13 @@ return {
       TelescopeSelection = { fg = "text", bg = "base" },
       TelescopeSelectionCaret = { fg = "rose", bg = "rose" },
     },
+
+    styles = {
+      transparency = true,
+    },
   },
   config = function(_, opts)
+    vim.o.termguicolors = true
     require("rose-pine").setup(opts)
 
     vim.cmd.colorscheme("rose-pine")
