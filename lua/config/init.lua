44124a9f70d08fd532d361439b16bd1dd8ac1ea1
jayzone91 <register@j4yz0n3.de>
2024-05-15 20:04:39 +0200

    syntax + lsp + formatters + linters

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -17,11 +17,34 @@ M.server = {
   },
   tailwindcss = {},
   tsserver = {},
+  css_variables = {},
+  cssls = {},
+  docker_compose_language_service = {},
+  dockerls = {},
+  emmet_ls = {},
+  gopls = {},
+  html = {},
+  intelephense = {},
+  jsonls = {},
+  marksman = {},
+  prismals = {},
+  pyright = {},
+  rust_analyzer = {
+    settings = {
+      ["rust-analyzer"] = {
+        diagnostics = {
+          enable = true,
+        },
+      },
+    },
+  },
+  taplo = {},
 }
 
 M.formatters = {
   "stylua", -- Format Lua
   "prettierd", -- format JS/TS/JSX/TSX/HTML/CSS etc
+  "black", -- format Python
 }
 
 M.linters = {
@@ -29,7 +52,12 @@ M.linters = {
   typescript = { "eslint_d" },
   javascriptreact = { "eslint_d" },
   typescriptreact = { "eslint_d" },
+  html = { "eslint_d" },
   lua = { "luacheck" },
+  json = { "jsonlint" },
+  jsonc = { "jsonlint" },
+  markdown = { "markdownlint" },
+  python = { "pylint" },
 }
 
 return M
