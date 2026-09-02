cf109b0ca0f10702b422b2115bb6f463535f0ed6
Johannes Kirchner <register@j4yz0n3.de>
2026-09-02 15:03:37 +0200

    Update autopairs.lua

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -7,6 +7,12 @@ local cmp = require("cmp")
 
 autopairs.setup({
 	check_ts = true,
+	map_cr = true,
+	map_bs = true,
+	map_c_h = true,
+	map_c_w = true,
+	enable_check_bracket_line = true,
+	enable_moveright = true,
 })
 
 cmp.event:on("confirm_done", require("nvim-autopairs.completion.cmp").on_confirm_done())
