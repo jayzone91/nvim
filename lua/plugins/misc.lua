72f315e94ce90cdcac8a5d509f4a14d55e693ede
Johannes Kirchner <register@j4yz0n3.de>
2026-09-02 11:23:34 +0200

    git signs

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -4,8 +4,10 @@ vim.pack.add({
 	"https://github.com/onsails/lspkind.nvim",
 	"https://github.com/nvim-lua/plenary.nvim",
 	"https://github.com/MunifTanjim/nui.nvim",
+	"https://github.com/lewis6991/gitsigns.nvim",
 })
 
 require("fidget").setup()
 require("mini.icons").setup()
 require("lspkind").init()
+require("gitsigns").setup()
