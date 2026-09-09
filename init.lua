b87fdbfa5b85963f94f2f67b934a043bbddde81a
Johannes Kirchner <register@j4yz0n3.de>
2026-09-09 17:50:33 +0200

    gitsigns  + context menu angefangen

--- init.lua
+++ init.lua
@@ -4,5 +4,6 @@ vim.g.maplocalleader = " "
 require("config.options")
 require("config.keymaps")
 require("config.autocmds")
+-- require("config.contextmenu")
 
 require("core.lazy")
---
+++ lua/config/contextmenu.lua
@@ -0,0 +1,27 @@
+local function menu(command)
+  vim.cmd(command)
+end
+
+-- Remove Neovim's default context menu
+pcall(vim.cmd, "aunmenu PopUp")
+
+-- Navigation
+menu([[nnoremenu PopUp.Go\ to\ Definition <Cmd>lua vim.lsp.buf.definition()<CR>]])
+menu([[nnoremenu PopUp.Show\ Diagnostics <Cmd>lua vim.diagnostic.open_float()<CR>]])
+
+menu([[amenu PopUp.-Navigation- <Nop>]])
+
+-- Clipboard
+menu([[vnoremenu PopUp.Cut "+x]])
+menu([[vnoremenu PopUp.Copy "+y]])
+
+menu([[nnoremenu PopUp.Paste "+gP]])
+menu([[vnoremenu PopUp.Paste "+P]])
+menu([[inoremenu PopUp.Paste <C-r>+]])
+
+menu([[amenu PopUp.-Clipboard- <Nop>]])
+
+-- Selection
+menu([[nnoremenu PopUp.Select\ All ggVG]])
+menu([[vnoremenu PopUp.Select\ All gg0oG$]])
+menu([[inoremenu PopUp.Select\ All <C-Home><C-o>VG]])
