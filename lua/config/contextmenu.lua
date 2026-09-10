5b804fa261c615f32aaab2678c5af046fbf841a5
Johannes Kirchner <register@j4yz0n3.de>
2026-09-10 09:09:29 +0200

    context

--- lua/config/contextmenu.lua
+++ lua/config/contextmenu.lua
@@ -2,26 +2,44 @@ local function menu(command)
 	vim.cmd(command)
 end
 
--- Remove Neovim's default context menu
-pcall(vim.cmd, "aunmenu PopUp")
+vim.opt.pumborder = "rounded"
 
--- Navigation
-menu([[nnoremenu PopUp.Go\ to\ Definition <Cmd>lua vim.lsp.buf.definition()<CR>]])
+vim.api.nvim_set_hl(0, "PmenuBorder", {
+	link = "FloatBorder",
+})
+
+pcall(vim.cmd, [[aunmenu PopUp.How-to\ disable\ mouse]])
+pcall(vim.cmd, [[aunmenu PopUp.-2-]])
+
+-- LSP Navigation
+menu([[nnoremenu PopUp.Go\ to\ References <Cmd>lua Snacks.picker.lsp_references()<CR>]])
+menu([[nnoremenu PopUp.Go\ to\ Implementation <Cmd>lua Snacks.picker.lsp_implementations()<CR>]])
+menu([[nnoremenu PopUp.Go\ to\ Type\ Definition <Cmd>lua Snacks.picker.lsp_type_definitions()<CR>]])
+
+-- Diagnostics
+menu([[anoremenu PopUp.-Diagnostics- <Nop>]])
+
 menu([[nnoremenu PopUp.Show\ Diagnostics <Cmd>lua vim.diagnostic.open_float()<CR>]])
+menu([[nnoremenu PopUp.Problems <Cmd>lua require("trouble").toggle("diagnostics")<CR>]])
 
-menu([[amenu PopUp.-Navigation- <Nop>]])
+-- Code
+menu([[anoremenu PopUp.-Code- <Nop>]])
 
--- Clipboard
-menu([[vnoremenu PopUp.Cut "+x]])
-menu([[vnoremenu PopUp.Copy "+y]])
+menu([[nnoremenu PopUp.Code\ Action <Cmd>lua vim.lsp.buf.code_action()<cr>]])
+menu([[vnoremenu PopUp.Code\ Action <Cmd>lua vim.lsp.buf.code_action()<cr>]])
 
-menu([[nnoremenu PopUp.Paste "+gP]])
-menu([[vnoremenu PopUp.Paste "+P]])
-menu([[inoremenu PopUp.Paste <C-r>+]])
+menu([[nnoremenu PopUp.Rename\ Symbol <Cmd>lua vim.lsp.buf.rename()<cr>]])
 
-menu([[amenu PopUp.-Clipboard- <Nop>]])
+-- Formatting
+menu([[anoremenu PopUp.-Format- <Nop>]])
 
--- Selection
-menu([[nnoremenu PopUp.Select\ All ggVG]])
-menu([[vnoremenu PopUp.Select\ All gg0oG$]])
-menu([[inoremenu PopUp.Select\ All <C-Home><C-o>VG]])
+menu([[anoremenu PopUp.Format <cmd>lua require("conform").format({lsp_format = "fallback"})<cr>]])
+menu([[vnoremenu PopUp.Format <cmd>lua require("conform").format({lsp_format = "fallback"})<cr>]])
+
+-- Git
+menu([[anoremenu PopUp.-Git- <Nop>]])
+
+menu([[nnoremenu PopUp.Preview\ Git\ Hunk <Cmd>lua require("gitsigns").preview_hunk()<CR>]])
+menu([[nnoremenu PopUp.Stage\ Git\ Hunk <Cmd>lua require("gitsigns").stage_hunk()<CR>]])
+menu([[nnoremenu PopUp.Reset\ Git\ Hunk <Cmd>lua require("gitsigns").reset_hunk()<CR>]])
+menu([[nnoremenu PopUp.Git\ Blame <Cmd>lua require("gitsigns").blame_line({ full = true })<CR>]])
