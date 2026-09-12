8b0f8f101f9d380a96d5757a2f33593d115dd5a6
Johannes Kirchner <register@j4yz0n3.de>
2026-09-12 20:02:10 +0200

    Update lint.lua

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -8,14 +8,11 @@ return {
 
       lint.linters_by_ft = linter
 
-      vim.api.nvim_create_autocmd(
-        { "BufEnter", "BufWritePost", "InsertLeave" },
-        {
-          callback = function()
-            lint.try_lint()
-          end,
-        }
-      )
+      vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost" }, {
+        callback = function()
+          lint.try_lint()
+        end,
+      })
     end,
   },
 }
