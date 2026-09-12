472f68185aade93baee49f0a442aaf81a4e46970
Johannes Kirchner <register@j4yz0n3.de>
2026-09-12 19:51:19 +0200

    Update keymaps.lua

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,8 +2,6 @@ local map = function(mode, key, func, desc)
   return vim.keymap.set(mode, key, func, { desc = desc or "" })
 end
 
-map({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", "Save File")
-
 map("n", "<leader>qq", "<cmd>qa<cr>", "Quit Neovim")
 
 vim.keymap.set({ "i", "n", "s" }, "<esc>", function()
