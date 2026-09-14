150325029004d3548f6f06ffb0cd1eddc9377fa1
Johannes Kirchner <register@j4yz0n3.de>
2026-09-14 10:42:15 +0200

    nvim-treesitter-textobjects

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -82,4 +82,44 @@ return {
       }
     end,
   },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    branch = "main",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("nvim-treesitter-textobjects").setup({
+        select = {
+          lookahead = true,
+        },
+        move = {
+          set_jumps = true,
+        },
+      })
+
+      local select = require("nvim-treesitter-textobjects.select")
+      local move = require("nvim-treesitter-textobjects.move")
+
+      vim.keymap.set({ "x", "o" }, "af", function()
+        select.select_textobject("@function.outer", "textobjects")
+      end, { desc = "Around Function" })
+
+      vim.keymap.set({ "x", "o" }, "if", function()
+        select.select_textobject("@function.inner", "textobjects")
+      end, { desc = "Inside Function" })
+
+      vim.keymap.set({ "x", "o" }, "aa", function()
+        select.select_textobject("@parameter.outer", "textobjects")
+      end, { desc = "Inside Argument" })
+
+      vim.keymap.set({ "n", "x", "o" }, "<A-C-Down>", function()
+        move.goto_next_start("@function.outer", "textobjects")
+      end, { desc = "Next Function" })
+
+      vim.keymap.set({ "n", "x", "o" }, "<A-C-Up>", function()
+        move.goto_previous_start("@function.outer", "textobjects")
+      end, { desc = "Previous Function" })
+    end,
+  },
 }
